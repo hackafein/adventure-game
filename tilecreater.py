e47e9bf3e303f222dd81e83aca46798d72073ecc
Furkan Ceran <42136880+hackafein@users.noreply.github.com>
2020-12-02 16:25:23 +0300

    last

--- tilecreater.py
+++ tilecreater.py
@@ -2,7 +2,26 @@ import pickle
 from os import path
 import random
 level=0
-
+def kenarlik(i,j):
+    v=random.randint(0,100)
+    if v<80:
+        deger=1
+    else:
+        deger=0
+    return deger
+def area(i,j):
+    v=random.randint(0,100)
+    if v<10:
+        deger=0
+    elif 10<v<20:
+        deger=1
+    elif 20<v<30:
+        deger=2
+    elif 95<v:
+        deger=7
+    else:
+        deger=random.randint(3,6)
+    return deger
 #pickle_in = open('/Users/furkanceran/Desktop/adventure-game/level0_data', 'rb')
 #world_data = pickle.load(pickle_in)
 #print(world_data)
@@ -10,8 +29,14 @@ liste=[]
 for i in range(0,20):
     liste.append([])
     for j in range(0,20):
-        deger=random.randint(0,7)
+        if i==0 or i==20 or j==0 or j==20:
+            deger=kenarlik(i,j)
+        else:
+            deger=area(i,j)
+        #deger=random.randint(0,7)
         liste[i].append(deger)
 
-with open('/Users/furkanceran/Desktop/adventure-game/level10_data', 'wb') as f:
+
+
+with open('/Users/furkanceran/Desktop/adventure-game/level0_data', 'wb') as f:
     pickle.dump(liste, f)
