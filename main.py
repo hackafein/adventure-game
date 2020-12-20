a3d13f68b70abb6202a1e91df420dbd344770926
Furkan Ceran <42136880+hackafein@users.noreply.github.com>
2020-12-20 21:22:11 +0300

    last

---
+++ main.py
@@ -0,0 +1,488 @@
+import pygame
+from pygame.locals import *
+from pygame import mixer
+import pickle
+from os import path
+import random
+#burada pygame mixer yani müzik oynatıcısını kuruyoruz
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+pygame.init()
+
+# pygame oyun içinde saniyede akacak frame sayısını belirliyoruz
+clock = pygame.time.Clock()
+fps = 60
+
+#Oyun pencere genisliği ve yüksekliğini ayarladık
+screen_width = 1000
+screen_height = 1000
+
+# ekranı olusturduk ve pencere baslığını belirledik
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Adventure game By Furkan Ceran')
+#Yazı fontunu ayarlıyoruz
+font = pygame.font.SysFont('Bauhaus 93', 70)
+font_score = pygame.font.SysFont('Bauhaus 93', 30)
+
+
+#oyuna dair değerleri burada tanımladım
+tile_size = 50
+game_over = 0
+main_menu = True
+level = 0
+max_levels = 999
+score = 0
+
+
+#renkleri belirliyorum ileride kullanıcağım
+white = (255, 255, 255)
+blue = (0, 0, 255)
+
+
+#resimleri yükledim
+sun_img = pygame.image.load('img/sun.png')
+bg_img = [pygame.image.load('img/sky.png'),pygame.image.load('img/purple.png'),pygame.image.load('img/mountain.png'),pygame.image.load('img/stars.png'),pygame.image.load('img/desert.png'),pygame.image.load('img/forest.png')]
+bg_selected=bg_img[random.randint(0,5)]
+grass_img_list=[pygame.image.load('img/grass.png'),pygame.image.load('img/redgrass.png'),pygame.image.load('img/beton.png')]
+grass_img = grass_img_list[random.randint(0,2)]
+dirt_img_list = [pygame.image.load('img/dirt.png'),pygame.image.load('img/betonblok.png'),pygame.image.load('img/wood.png'),pygame.image.load('img/sandblock.png')]
+dirt_img = dirt_img_list[random.randint(0,3)]
+restart_img = pygame.image.load('img/restart_btn.png')
+start_img = pygame.image.load('img/start_btn.png')
+exit_img = pygame.image.load('img/exit_btn.png')
+
+#müzikleri yüklüyorum
+pygame.mixer.music.load('img/music.wav')
+pygame.mixer.music.play(-1, 0.0, 5000)
+coin_fx = pygame.mixer.Sound('img/coin.wav')
+coin_fx.set_volume(0.5)
+jump_fx = pygame.mixer.Sound('img/jump.wav')
+jump_fx.set_volume(0.5)
+game_over_fx = pygame.mixer.Sound('img/game_over.wav')
+game_over_fx.set_volume(0.5)
+
+
+def draw_text(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
+
+# leveli resetliyeceğim fonksiyonu yazdım
+def reset_level(level):
+	player.reset(100, screen_height - 130)
+	blob_group.empty()
+	platform_group.empty()
+	coin_group.empty()
+	lava_group.empty()
+	exit_group.empty()
+
+	# burası levele göre dünyayı pickle formatında yükleyip olusturacak kısım
+	if path.exists(f'level{level}_data'):
+		pickle_in = open(f'level{level}_data', 'rb')
+		world_data = pickle.load(pickle_in)
+	world = World(world_data)
+	#skoru yazdıracak yeri ekran da belirledim sol üste yazdırıcağım
+	score_coin = Coin(tile_size // 2, tile_size // 2)
+	coin_group.add(score_coin)
+	return world
+
+
+class Button():
+	def __init__(self, x, y, image):
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.clicked = False
+
+	def draw(self):
+		action = False
+
+		#mouse pozisyonlarını alıyorum
+		pos = pygame.mouse.get_pos()
+
+		# Mouse tıklandı mı kontrolü
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				action = True
+				self.clicked = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+
+		#dbutonu çizdirdim
+		screen.blit(self.image, self.rect)
+
+		return action
+
+
+class Player():
+	def __init__(self, x, y):
+		self.reset(x, y)
+
+	def update(self, game_over):
+		dx = 0
+		dy = 0
+		walk_cooldown = 5
+		col_thresh = 20
+
+		if game_over == 0:
+			#Butonların basılıp basılmadığını algıladığımız kısım
+			key = pygame.key.get_pressed()
+			if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+				jump_fx.play()
+				self.vel_y = -15
+				self.jumped = True
+			if key[pygame.K_SPACE] == False:
+				self.jumped = False
+			if key[pygame.K_LEFT]:
+				dx -= 5
+				self.counter += 1
+				self.direction = -1
+			if key[pygame.K_RIGHT]:
+				dx += 5
+				self.counter += 1
+				self.direction = 1
+			if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+				self.counter = 0
+				self.index = 0
+				if self.direction == 1:
+					self.image = self.images_right[self.index]
+				if self.direction == -1:
+					self.image = self.images_left[self.index]
+
+
+			# Yürüme animasyonunu burada olusturdum
+			if self.counter > walk_cooldown:
+				self.counter = 0	
+				self.index += 1
+				if self.index >= len(self.images_right):
+					self.index = 0
+				if self.direction == 1:
+					self.image = self.images_right[self.index]
+				if self.direction == -1:
+					self.image = self.images_left[self.index]
+
+
+			#yerçekimi ekledim
+			self.vel_y += 1
+			if self.vel_y > 10:
+				self.vel_y = 10
+			dy += self.vel_y
+
+			# Çapısma var mı onu belirledim
+			self.in_air = True
+			for tile in world.tile_list:
+				# burası x eksenindeki çarpısmaları kontrol eder
+				if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+					dx = 0
+				#burası y eksenindeki çarpısmaları kontrol eder
+				if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+					#zıplama durumundaki çarpmanın sonucu düseriz
+					if self.vel_y < 0:
+						dy = tile[1].bottom - self.rect.top
+						self.vel_y = 0
+					#zemine düsersek dururuz
+					elif self.vel_y >= 0:
+						dy = tile[1].top - self.rect.bottom
+						self.vel_y = 0
+						self.in_air = False
+
+
+			#düsmanla olan çarpısmayı burada kontrol ediyorum
+			if pygame.sprite.spritecollide(self, blob_group, False):
+				game_over = -1
+				game_over_fx.play()
+
+			#lav ile olan çarpısmayı burada kontrol ediyorum
+			if pygame.sprite.spritecollide(self, lava_group, False):
+				game_over = -1
+				game_over_fx.play()
+
+			#çıkısa girip girmediğimi burada kontrol ediyorum
+			if pygame.sprite.spritecollide(self, exit_group, False):
+				game_over = 1
+
+
+			#platormla olan çarpısmalar
+			for platform in platform_group:
+				#x ekseni
+				if platform.rect.colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+					dx = 0
+				#y ekseni
+				if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+					#asağı
+					if abs((self.rect.top + dy) - platform.rect.bottom) < col_thresh:
+						self.vel_y = 0
+						dy = platform.rect.bottom - self.rect.top
+					#yukarı
+					elif abs((self.rect.bottom + dy) - platform.rect.top) < col_thresh:
+						self.rect.bottom = platform.rect.top - 1
+						self.in_air = False
+						dy = 0
+					
+					if platform.move_x != 0:
+						self.rect.x += platform.move_direction
+
+
+			#oyuncu koordinatlarını güncelledim
+			self.rect.x += dx
+			self.rect.y += dy
+
+
+		elif game_over == -1:
+			self.image = self.dead_image
+			draw_text('Kaybettin !', font, blue, (screen_width // 2) - 200, screen_height // 2)
+			if self.rect.y > 200:
+				self.rect.y -= 5
+
+		#oyuncuyu ekrana çizdirdim
+		screen.blit(self.image, self.rect)
+
+		return game_over
+
+
+	def reset(self, x, y):
+		self.images_right = []
+		self.images_left = []
+		self.index = 0
+		self.counter = 0
+		for num in range(1, 5):
+			img_right = pygame.image.load(f'img/guy{num}.png')
+			img_right = pygame.transform.scale(img_right, (40, 80))
+			img_left = pygame.transform.flip(img_right, True, False)
+			self.images_right.append(img_right)
+			self.images_left.append(img_left)
+		self.dead_image = pygame.image.load('img/ghost.png')
+		self.image = self.images_right[self.index]
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
+		self.vel_y = 0
+		self.jumped = False
+		self.direction = 0
+		self.in_air = True
+
+
+
+class World():
+	def __init__(self, data):
+		self.tile_list = []
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				if tile == 1:
+					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				if tile == 2:
+					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				if tile == 3:
+					blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+					blob_group.add(blob)
+				if tile == 4:
+					platform = Platform(col_count * tile_size, row_count * tile_size, 1, 0)
+					platform_group.add(platform)
+				if tile == 5:
+					platform = Platform(col_count * tile_size, row_count * tile_size, 0, 1)
+					platform_group.add(platform)
+				if tile == 6:
+					lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+					lava_group.add(lava)
+				if tile == 7:
+					coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+					coin_group.add(coin)
+				if tile == 8:
+					exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+					exit_group.add(exit)
+				col_count += 1
+			row_count += 1
+	def draw(self):
+		for tile in self.tile_list:
+			screen.blit(tile[0], tile[1])
+
+
+
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('img/blob.png')
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_direction = 1
+		self.move_counter = 0
+
+	def update(self):
+		self.rect.x += self.move_direction
+		self.move_counter += 1
+		if abs(self.move_counter) > 50:
+			self.move_direction *= -1
+			self.move_counter *= -1
+
+
+class Platform(pygame.sprite.Sprite):
+	def __init__(self, x, y, move_x, move_y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/platform.png')
+		self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_counter = 0
+		self.move_direction = 1
+		self.move_x = move_x
+		self.move_y = move_y
+
+
+	def update(self):
+		self.rect.x += self.move_direction * self.move_x
+		self.rect.y += self.move_direction * self.move_y
+		self.move_counter += 1
+		if abs(self.move_counter) > 50:
+			self.move_direction *= -1
+			self.move_counter *= -1
+
+
+
+
+
+class Lava(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/lava.png')
+		self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+
+class Coin(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/coin.png')
+		self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+
+class Exit(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/exit.png')
+		self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+
+
+player = Player(100, screen_height - 130)
+
+blob_group = pygame.sprite.Group()
+platform_group = pygame.sprite.Group()
+lava_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+
+#skore yazılacak yeri belirledim ve yazdırdım
+score_coin = Coin(tile_size // 2, tile_size // 2)
+coin_group.add(score_coin)
+
+#levellerin verisini yükle ve dünyayı olustur
+if path.exists(f'level{level}_data'):
+	pickle_in = open(f'level{level}_data', 'rb')
+	world_data = pickle.load(pickle_in)
+world = World(world_data)
+
+
+#buton yaratır
+restart_button = Button(screen_width // 2 - 50, screen_height // 2 + 100, restart_img)
+start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 150, screen_height // 2, exit_img)
+
+
+run = True
+while run:
+
+	clock.tick(fps)
+
+	screen.blit(bg_selected, (0, 0))
+	screen.blit(sun_img, (100, 100))
+
+	if main_menu == True:
+		if exit_button.draw():
+			run = False
+		if start_button.draw():
+			main_menu = False
+	else:
+		world.draw()
+
+		if game_over == 0:
+			blob_group.update()
+			platform_group.update()
+			#skoru günceller
+			# coin toplandı mı kontrol et toplandıysa skoru artır
+			if pygame.sprite.spritecollide(player, coin_group, True):
+				score += 1
+				coin_fx.play()
+			draw_text('X ' + str(score), font_score, white, tile_size - 10, 10)
+		
+		blob_group.draw(screen)
+		platform_group.draw(screen)
+		lava_group.draw(screen)
+		coin_group.draw(screen)
+		exit_group.draw(screen)
+
+		game_over = player.update(game_over)
+
+		#eğer oyuncu öldüyse
+		if game_over == -1:
+			if restart_button.draw():
+				world_data = []
+				world = reset_level(level)
+				game_over = 0
+				score = 0
+
+		#eğer oyuncu leveli bitirdiyse kapıya ulastıysa
+		if game_over == 1:
+			#Oyunu sıfırla leveli 1 artır
+			level += 1
+			bg_selected=bg_img[random.randint(0,5)]
+			grass_img = grass_img_list[random.randint(0,2)]
+			dirt_img=dirt_img_list[random.randint(0,3)]
+
+			if level <= max_levels:
+				#Leveli resetler
+				world_data = []
+				world = reset_level(level)
+				game_over = 0
+			else:
+				draw_text('KAZANDIN!!', font, blue, (screen_width // 2) - 140, screen_height // 2)
+				if restart_button.draw():
+					level = 1
+					#Leveli resetler
+					world_data = []
+					world = reset_level(level)
+					game_over = 0
+					score = 0
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+
+	pygame.display.update()
+
+pygame.quit()
